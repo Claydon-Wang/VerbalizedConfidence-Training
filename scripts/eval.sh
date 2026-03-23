424064146e3b6877dc6bc51604a2738be22882c5
Shuoyuan Wang <claytonwang0205@gmail.com>
2026-03-23 04:15:14 +0000

    fix scripts

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -6,28 +6,23 @@ GPU=7
 INFERENCER=verbalized_confidence
 
 DATASETS=(
-  # Factual
   "Hotpot"
-  # "HotpotVanilla"
-  # "SimpleQA"
-  # "TriviaQA"
-  # "CommonsenseQA"
-  # "GPQA"
-  # "MMLUPro"
-
-  # Math
-  # "GSM8K"
-  # "Math500"
-
-  # Code
+  "HotpotVanilla"
+  "SimpleQA"
+  "TriviaQA"
+  "CommonsenseQA"
+  "GPQA"
+  "MMLUPro"
+  "GSM8K"
+  "Math500"
 )
 
+# different methods
 for dataset in "${DATASETS[@]}"; do
   CUDA_VISIBLE_DEVICES=$GPU python -m src.eval.eval_main --dataset "$dataset" --model Qwen25_1_5B_Instruct --inferencer "$INFERENCER"
-
-  # CUDA_VISIBLE_DEVICES=$GPU python -m src.eval.eval_main --dataset "$dataset" --model Qwen25_1_5B_Instruct --inferencer "$INFERENCER" --policy HotpotRLVR --checkpoint /mnt/sharedata/ssd_large/users/wsy/project/rl/RLCR/temp/train/RLVR-hotpot
-  # CUDA_VISIBLE_DEVICES=$GPU python -m src.eval.eval_main --dataset "$dataset" --model Qwen25_1_5B_Instruct --inferencer "$INFERENCER" --policy HotpotRLCR --checkpoint /mnt/sharedata/ssd_large/users/wsy/project/rl/RLCR/temp/train/RLCR-base-hotpot
-  # CUDA_VISIBLE_DEVICES=$GPU python -m src.eval.eval_main --dataset "$dataset" --model Qwen25_1_5B_Instruct --inferencer "$INFERENCER" --policy HotpotCoCA --checkpoint /path/to/hotpot-coca-checkpoint
+  CUDA_VISIBLE_DEVICES=$GPU python -m src.eval.eval_main --dataset "$dataset" --model Qwen25_1_5B_Instruct --inferencer "$INFERENCER" --policy HotpotRLVR --checkpoint logs/train/hotpot/rlvr/qwen25_1_5b_instruct/2026-0322-0454
+  CUDA_VISIBLE_DEVICES=$GPU python -m src.eval.eval_main --dataset "$dataset" --model Qwen25_1_5B_Instruct --inferencer "$INFERENCER" --policy HotpotRLCR --checkpoint logs/train/hotpot/rlcr/qwen25_1_5b_instruct/2026-0322-0450
+  CUDA_VISIBLE_DEVICES=$GPU python -m src.eval.eval_main --dataset "$dataset" --model Qwen25_1_5B_Instruct --inferencer "$INFERENCER" --policy HotpotCoCA --checkpoint logs/train/hotpot/coca/qwen25_1_5b_instruct/2026-0322-1101
 
   # CUDA_VISIBLE_DEVICES=$GPU python -m src.eval.eval_main --dataset "$dataset" --model Qwen25_1_5B_Instruct --inferencer "$INFERENCER" --policy MathRLVR --checkpoint /path/to/math-rlvr-checkpoint
   # CUDA_VISIBLE_DEVICES=$GPU python -m src.eval.eval_main --dataset "$dataset" --model Qwen25_1_5B_Instruct --inferencer "$INFERENCER" --policy MathRLCR --checkpoint /path/to/math-rlcr-checkpoint
